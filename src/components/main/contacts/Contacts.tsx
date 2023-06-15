b7a0209fff5f832d351088b6c962159b13eb1505
Ganchenko-Ilya <chicha3d@gmail.com>
2023-06-15 14:06:17 +0300

    Fix component Contacts

--- src/components/main/contacts/Contacts.tsx
+++ src/components/main/contacts/Contacts.tsx
@@ -4,7 +4,7 @@ import sContainer from "./../../../common/style/container.module.css";
 export const Contacts = () => {
   return (
     <div className={s.contacts}>
-      <div className={sContainer.container }>
+      <div className={sContainer.container + ' ' + s.contactsWrapper }>
         <h2>Контакты</h2>
         <form action="" className={s.wrapper}>
           <input type="text" placeholder="Name" />
